eea9e1ee9b854c0faf495af622960c79155db63e
Gyanendra <sharmagyanendra874@gmail.com>
2026-06-07 07:10:07 +0530

    ci: Add GitHub Pages deployment workflow

--- obiwan-dashboard/vite.config.js
+++ obiwan-dashboard/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Estimating-Biomass-Change-with-GEDI-and-the-OBIWAN-API/',
   plugins: [react()],
   server: {
     proxy: {
